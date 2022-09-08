89a3711b24e1f69102ff4133dfd5ceba2cc9228d
Kingsobec <107012740+Kingsobec@users.noreply.github.com>
2022-09-08 23:33:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,314 @@
+let teamMembers = [
+  {
+    id: 1,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+  {
+    id: 2,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+  {
+    id: 3,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+  {
+    id: 4,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+  {
+    id: 5,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+  {
+    id: 6,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+  {
+    id: 7,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+  {
+    id: 8,
+    name: `lorem`,
+    img: `./images/goku.png`,
+    description: `Lorem tincidunt integer pellentesque nunc varius lorem`,
+  },
+];
+let nft = [
+  {
+    id: 1,
+    img: `./images/Grey-Cat-Ready-high.png`,
+    name: `lee`,
+  },
+  {
+    id: 2,
+    img: `./images/big-puffy-jacketbleck-1.png`,
+    name: `filix`,
+  },
+  {
+    id: 3,
+    img: `./images/Cool-Cat-Ready-High.png`,
+    name: `lorem`,
+  },
+  {
+    id: 4,
+    img: `./images/REDCAT.png`,
+    name: `goku`,
+  },
+  {
+    id: 5,
+    img: `./images/Grey-Cat-Ready-high.png`,
+    name: `lee`,
+  },
+  {
+    id: 6,
+    img: `./images/big-puffy-jacketbleck-1.png`,
+    name: `filix`,
+  },
+  {
+    id: 7,
+    img: `./images/Cool-Cat-Ready-High.png`,
+    name: `lorem`,
+  },
+  {
+    id: 8,
+    img: `./images/REDCAT.png`,
+    name: `goku`,
+  },
+];
+let questions = [
+  {
+    question: `Aliquam porta nisl dolor, molestie pellentesque elit molestie in. congue semper lacus, facilisis ?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem
+    venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida.
+    Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    question: `pellentesque elit molestie in. congue semper lacus, facilisis ?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem
+    venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida.
+    Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    question: `Aliquam porta nisl dolor, molestie?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem
+    venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida.
+    Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    question: `congue semper lacus, facilisis ?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem
+    venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida.
+    Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    question: `congue semper lacus, facilisis ?`,
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem
+    venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida.
+    Vitae id tristique arcu cursus purus.`,
+  },
+];
+
+let utContainer = [
+  {
+    id: 01,
+    name: `seasonal rewards`,
+    detail: `This will show a text about seasonal rewards Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida. Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    id: 02,
+    name: `irl resort`,
+    detail: `This will show a text about irl resort Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida. Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    id: 03,
+    name: `raffle`,
+    detail: `This will show a text about raffle Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida. Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    id: 04,
+    name: `$cyard`,
+    detail: `This will show a text about $cyard Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida. Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    id: 05,
+    name: `community`,
+    detail: `This will show a text about community Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida. Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    id: 06,
+    name: `merch`,
+    detail: `This will show a text about merch Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem tincidunt integer pellentesque nunc varius lorem venenatis diam. Urna, quam quam urna, in enim morbi in. Vel nulla pulvinar sed nunc porttitor diam, magna ut gravida. Vitae id tristique arcu cursus purus.`,
+  },
+  {
+    id: 07,
+    name: `more to come`,
+    detail: ``,
+  },
+];
+
+const teamSection = document.querySelector(".team-members");
+const nftSection = document.querySelector(".nft-images");
+const showQuestions = document.querySelector(".questions");
+const showAnswer = document.querySelector(".answer");
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayteamDetails(teamMembers);
+  displayNftDetails(nft);
+  // answers(questions)
+});
+
+function displayteamDetails(teamDetails) {
+  let displayTeam = teamDetails.map(function (item) {
+    // console.log(item)
+    return `<div class="team-member">
+              <img src="${item.img}" alt="">
+              <p class="team-name">${item.name}</p>
+              <p class="team-desc">${item.description}</p>
+            </div>`;
+  });
+  displayTeam = displayTeam.join("");
+  teamSection.innerHTML = displayTeam;
+}
+
+function displayNftDetails(nftDetails) {
+  let displayNft = nftDetails.map(function (item) {
+    return `<div class="nft">
+              <img src="${item.img}" alt="">
+            </div>`;
+  });
+  displayNft = displayNft.join("");
+  nftSection.innerHTML = displayNft;
+}
+
+// function answers(qAndA) {
+//   let displayQuestions = qAndA.map(function (item) {
+//     // console.log(item)
+//     return `<div class="question-body">
+//             <div class="main-questions">
+//               <div class="ask">
+//                 <p>${item.question}</p>
+//                 <button type="button" class="shower">
+//                   <i class="fa-solid fa-caret-up"></i>
+//                 </button>
+//               </div>
+//               <div class="answer">
+//                 <p>${item.answer}</p>
+//               </div>
+//             </div>
+//           </div>`;
+//   });
+//   displayQuestions = displayQuestions.join("");
+//   showQuestions.innerHTML = displayQuestions;
+// }
+
+const selector = document.querySelectorAll(".question-body");
+const btns = document.querySelectorAll(".shower");
+// btns.forEach(function(btn){
+//   btn.addEventListener('click', function(e){
+//     const show = e.currentTarget.parentElement.parentElement;
+//     btn.classList.toggle('rotate')
+//     show.classList.toggle('show-answer')
+//   })
+// })
+
+selector.forEach(function (question) {
+  const answer = question.querySelector(".shower");
+  const icons = question.querySelector("i");
+  answer.addEventListener("click", function () {
+    selector.forEach(function (oracle) {
+      if (oracle !== question) {
+        oracle.classList.remove("show-answer");
+        const icon = oracle.querySelector("i");
+        icon.classList.remove("fa-caret-up");
+      }
+    });
+    icons.classList.toggle("fa-caret-up");
+    question.classList.toggle("show-answer");
+  });
+});
+
+const hide = document.querySelector(".left");
+const hide2 = document.querySelector(".right");
+const utility = document.querySelectorAll(".ut-border");
+const closer = document.querySelector(".show-utilities");
+const closeBtn = document.querySelector(".close");
+const namer = document.querySelector(".utilityname");
+const utDetail = document.querySelector(".utility-detail");
+const utNum = document.querySelector(".ut-num");
+const show1 = document.querySelector("#show1");
+const show2 = document.querySelector("#show2");
+const show3 = document.querySelector("#show3");
+const show4 = document.querySelector("#show4");
+const show5 = document.querySelector("#show5");
+const show6 = document.querySelector("#show6");
+const show7 = document.querySelector("#show7");
+
+utility.forEach(function (show) {
+  show.addEventListener("click", function (ex) {
+    closer.style.display = "block";
+    hide.classList.add("filter");
+    hide2.classList.add("filter");
+  });
+});
+
+closeBtn.addEventListener("click", function () {
+  closer.style.display = "none";
+  hide.classList.remove("filter");
+  hide2.classList.remove("filter");
+});
+
+show1.addEventListener("click", function () {
+  namer.textContent = `${utContainer[0].name}`;
+  utDetail.textContent = `${utContainer[0].detail}`;
+  utNum.textContent = `${utContainer[0].id}`;
+});
+
+show3.addEventListener("click", function () {
+  namer.textContent = `${utContainer[2].name}`;
+  utDetail.textContent = `${utContainer[2].detail}`;
+  utNum.textContent = `${utContainer[2].id}`;
+});
+
+show4.addEventListener("click", function () {
+  namer.textContent = `${utContainer[3].name}`;
+  utDetail.textContent = `${utContainer[3].detail}`;
+  utNum.textContent = `${utContainer[3].id}`;
+});
+
+show5.addEventListener("click", function () {
+  namer.textContent = `${utContainer[4].name}`;
+  utDetail.textContent = `${utContainer[4].detail}`;
+  utNum.textContent = `${utContainer[4].id}`;
+});
+
+show6.addEventListener("click", function () {
+  namer.textContent = `${utContainer[5].name}`;
+  utDetail.textContent = `${utContainer[5].detail}`;
+  utNum.textContent = `${utContainer[5].id}`;
+});
+
+show2.addEventListener("click", function () {
+  namer.textContent = `${utContainer[1].name}`;
+  utDetail.textContent = `${utContainer[1].detail}`;
+  utNum.textContent = `${utContainer[1].id}`;
+});
+show7.addEventListener("click", function () {
+  closer.style.display = "none";
+  hide.classList.remove("filter");
+  hide2.classList.remove("filter");
+});
